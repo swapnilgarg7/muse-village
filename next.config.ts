797b73596b79c8e7ad0cdc14d41ebe9abe007666
swapnilgarg7 <swapnilgarg810@gmail.com>
2026-05-30 11:02:50 +0530

    refactor: format next.config.ts for improved readability and structure fix: update react-toastify to version 11.1.0 in package.json and pnpm-lock.yaml

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,32 @@
 
-      /** @type {import("next").NextConfig} */
-      const config = {
-        trailingSlash: true,
-        images: {
-          unoptimized: true,
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: '*',
-              pathname: '**',
-            },
-          ],
-        },
-typescript: {
-          ignoreBuildErrors: true,
-        },
-      };
-      export default config;
+/** @type {import("next").NextConfig} */
+const config = {
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
+export default config;
