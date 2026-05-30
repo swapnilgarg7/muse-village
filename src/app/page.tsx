9bb2bcb4b239821d2da54f6b68e1237f70608c1b
swapnilgarg7 <swapnilgarg810@gmail.com>
2026-05-30 10:53:22 +0530

    removed testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
                 <StatsBar />
                 <HowItWorks />
                 <Features />
-                <Testimonials />
                 <DonationSection />
                 <Footer />
             </main>
